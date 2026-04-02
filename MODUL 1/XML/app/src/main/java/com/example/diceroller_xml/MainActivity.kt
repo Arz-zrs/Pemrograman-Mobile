5bda96340c4842744a52093a481153a7c7779c25
Arya Arrozza Ridho Syaputra <ozzaboy123@gmail.com>
2026-04-02 20:19:38 +0800

    snackbar implemented

--- MODUL 1/XML/app/src/main/java/com/example/diceroller_xml/MainActivity.kt
+++ MODUL 1/XML/app/src/main/java/com/example/diceroller_xml/MainActivity.kt
@@ -3,23 +3,22 @@ package com.example.diceroller_xml
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var diceImage: ImageView
     lateinit var diceImage2: ImageView
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
             rollDice()
         }
-
         diceImage = findViewById(R.id.dice_image)
         diceImage2 = findViewById(R.id.dice_image2)
     }
@@ -50,5 +49,13 @@ class MainActivity : AppCompatActivity() {
 
         diceImage2.setImageResource(imageResource2)
         diceImage2.contentDescription = result2.toString()
+
+        val message = if (result1 == result2) getString(R.string.the_double)
+        else getString(R.string.unlucky)
+
+        val layout: LinearLayout = findViewById(R.id.layout)
+        val snackbar = Snackbar
+            .make(layout, message, Snackbar.LENGTH_LONG)
+        snackbar.show()
     }
 }
