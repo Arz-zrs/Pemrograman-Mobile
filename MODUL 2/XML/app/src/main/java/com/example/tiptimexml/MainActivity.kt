0d3a86f438ff4eb93116ca4ccfa7e61eff8f8179
Arya Arrozza Ridho Syaputra <ozzaboy123@gmail.com>
2026-04-16 20:11:11 +0800

    completed XML tip calc, could do some refac maybe later

--- MODUL 2/XML/app/src/main/java/com/example/tiptimexml/MainActivity.kt
+++ MODUL 2/XML/app/src/main/java/com/example/tiptimexml/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.tiptimexml
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.materialswitch.MaterialSwitch
+import com.google.android.material.textfield.TextInputEditText
+import java.text.NumberFormat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var numberField : TextInputEditText
+    private lateinit var tipOptions : AutoCompleteTextView
+    private lateinit var roundUpSwitch : MaterialSwitch
+    private lateinit var tipAmount: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +28,36 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        numberField = findViewById(R.id.number_field)
+        tipOptions = findViewById(R.id.tip_options)
+        roundUpSwitch = findViewById(R.id.round_up_switch)
+        tipAmount = findViewById(R.id.tip_amount)
+
+        val tipList = listOf(
+            getString(R.string.tip_option_1),
+            getString(R.string.tip_option_2),
+            getString(R.string.tip_option_3)
+        )
+        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, tipList)
+        tipOptions.setAdapter(adapter)
+
+        tipOptions.setOnItemClickListener { _, _, _, _ -> calculateTip() }
+        roundUpSwitch.setOnCheckedChangeListener { _, _ -> calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val billAmount = numberField.text.toString().toDoubleOrNull() ?: 0.0
+        val tipPercentage = when (tipOptions.text.toString()) {
+            getString(R.string.tip_option_1) -> 0.15
+            getString(R.string.tip_option_2) -> 0.18
+            else -> 0.2
+        }
+
+        var tip = billAmount * tipPercentage
+        if (roundUpSwitch.isChecked) tip = kotlin.math.ceil(tip)
+
+        val formatTip = NumberFormat.getCurrencyInstance(Locale.US).format(tip)
+        tipAmount.text = getString(R.string.tip_amount, formatTip)
     }
 }
