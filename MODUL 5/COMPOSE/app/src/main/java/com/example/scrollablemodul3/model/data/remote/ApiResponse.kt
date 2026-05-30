6745a90ee8cd3d4f1d572b15179644a39a310d03
Arya Arrozza Ridho Syaputra <ozzaboy123@gmail.com>
2026-05-30 19:38:58 +0800

    refac: refactored ApiResponse to sealed interface

--- MODUL 5/COMPOSE/app/src/main/java/com/example/scrollablemodul3/model/data/remote/ApiResponse.kt
+++ MODUL 5/COMPOSE/app/src/main/java/com/example/scrollablemodul3/model/data/remote/ApiResponse.kt
@@ -1,11 +1,11 @@
 package com.example.scrollablemodul3.model.data.remote
 
-sealed class ApiResponse<out T> {
-    data class Success<out T>(val data: T) : ApiResponse<T>()
+sealed interface ApiResponse<out T> {
+    data class Success<out T>(val data: T) : ApiResponse<T>
     data class Error(
         val message: String? = null,
         val throwable: Throwable? = null,
         val statusCode: Int? = null
-    ) : ApiResponse<Nothing>()
-    data object Loading : ApiResponse<Nothing>()
+    ) : ApiResponse<Nothing>
+    data object Loading : ApiResponse<Nothing>
 }
